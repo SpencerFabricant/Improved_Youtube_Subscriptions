60a76f4bd1afe313c8bc3c67a4d5a5b1971be9e0
Spencer Fabricant <spencer.fabricant@gmail.com>
2025-02-11 14:31:33 -0500

    WIP

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@
 
 import os
 
+import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 from datetime import datetime
@@ -16,11 +17,15 @@ def get_youtube():
     api_key = os.environ['GOOGLE_API_KEY']
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    client_secrets_file = os.path.expanduser("~/.google_client/improved_subscriptions_secret.json")
 
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_local_server()
     youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, developerKey=api_key)
-    
+        api_service_name, api_version, credentials=credentials)
+
     return youtube
 
 def get_channel_id(channel_name):
